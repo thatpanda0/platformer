885351d59c3948fac2efb1c150b592bcb3a865b9
That Panda <165739041+thatpanda0@users.noreply.github.com>
2025-07-10 11:00:22 -0700

    skibidi ribbidi

--- sketch.js
+++ sketch.js
@@ -1,17 +1,23 @@
-
 let player;
 let platforms = [];
+let spikes = [];
 let camX = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   player = new Player(100, 300, 15);
+
   platforms.push(new Platform(-320, height - 40, 520, 40, 0));
-  platforms.push(new Platform(300, height - 150, 90, 40, 0));
+  platforms.push(new Platform(300, height - 150, 40, 40, 0));
   platforms.push(new Platform(600, height - 290, 120, 60, 0));
-  platforms.push(new Platform(1000, height - 200, 200, 20, 0));
+  platforms.push(new Platform(1000, height - 200, 200, 40, 0));
   platforms.push(new Platform(1400, height - 300, 200, 2000, 0));
-  platforms.push(new Platform(1590, height - 40, 200, 40, 0));
+  platforms.push(new Platform(1590, height - 50, 660, 50, 0));
+  platforms.push(new Platform(1810, height - 460, 200, 200, 0));
+  platforms.push(new Platform(2249, height - 201, 200, 2000, 0));
+
+  spikes.push(new Spike(2000, height - 50, 50));
+  spikes.push(new Spike(690, height - 290, 30));
 }
 
 function draw() {
@@ -23,23 +29,28 @@ function draw() {
   player.update();
   player.show();
 
-
   for (let plat of platforms) {
     plat.show();
     plat.checkCollision(player);
   }
-  
-    if (keyIsDown(32) || keyIsDown(38) || keyIsDown(87)) {
-      player.jump();
-    }
-  
-  camX = player.pos.x - 200;
+
+  for (let sp of spikes) {
+    sp.show();
+    sp.checkCollision(player);
+  }
+
+  if (keyIsDown(32) || keyIsDown(38) || keyIsDown(87)) {
+    player.jump();
+  }
+
+  camX = player.pos.x - 400;
   camX = constrain(camX, 0, 6000 - width);
 }
 
 
 class Player {
   constructor(x, y, r) {
+    this.spawn = createVector(x, y);
     this.pos = createVector(x, y);
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0.65);
@@ -47,33 +58,29 @@ class Player {
     this.h = 40;
     this.r = r;
     this.onGround = false;
-    this.onWall = false;
-    this.wallDir = 0; // -1 left, 1 right
+  }
+
+  reset() {
+    this.pos.set(this.spawn);
+    this.vel.set(0, 0);
   }
 
   update() {
     if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) this.vel.x += 1;
-    if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) this.vel.x -= 1;
+    if (keyIsDown(LEFT_ARROW)  || keyIsDown(65)) this.vel.x -= 1;
 
     this.vel.add(this.acc);
     this.pos.add(this.vel);
-
     this.onGround = false;
-    this.onWall = false;
 
     if (this.pos.y > height) {
-      this.pos.y = height;
-      this.vel.y = 0;
-      this.onGround = true;
+      player.reset();
     }
   }
 
   jump() {
     if (this.onGround) {
       this.vel.y = -15;
-    } else if (this.onWall) {
-      this.vel.y = -12;
-      this.vel.x = this.wallDir * 12; // push away
     }
   }
 
@@ -82,6 +89,7 @@ class Player {
     rect(this.pos.x, this.pos.y, this.w, this.h, this.r);
   }
 }
+
 
 class Platform {
   constructor(x, y, w, h, r) {
@@ -97,19 +105,16 @@ class Platform {
   }
 
   checkCollision(player) {
-    // aabb collision
     if (player.pos.x < this.pos.x + this.w &&
         player.pos.x + player.w > this.pos.x &&
         player.pos.y < this.pos.y + this.h &&
         player.pos.y + player.h > this.pos.y) {
           
-      // centre points
       let p_centerX = player.pos.x + player.w / 2;
       let plat_centerX = this.pos.x + this.w / 2;
       let p_centerY = player.pos.y + player.h / 2;
       let plat_centerY = this.pos.y + this.h / 2;
 
-      // centre point difference
       let diffX = p_centerX - plat_centerX;
       let diffY = p_centerY - plat_centerY;
 
@@ -124,8 +129,8 @@ class Platform {
 
       if (penetrationX < penetrationY) {
         player.onWall = true;
-        if (player.vel.y > 1) {
-            player.vel.y = 1;
+        if (player.vel.y > 0.75) {
+            player.vel.y = 0.75;
         }
 
         if (diffX > 0) {
@@ -148,5 +153,65 @@ class Platform {
         }
       }
     }
+  }
+}
+
+
+class Spike {
+  constructor(x, baseY, size) {
+    this.x = x;
+    this.size = size;
+    this.h = size * sqrt(3) / 2;
+    this.y = baseY;
+    this.verts = [
+      createVector(x, baseY),
+      createVector(x + size, baseY),
+      createVector(x + size/2, baseY - this.h)
+    ];
+  }
+
+  show() {
+    fill(0);
+    noStroke();
+    triangle(
+      this.verts[0].x, this.verts[0].y,
+      this.verts[1].x, this.verts[1].y,
+      this.verts[2].x, this.verts[2].y
+    );
+  }
+
+  checkCollision(player) {
+    // are corners inside triangle?
+    let corners = [
+      createVector(player.pos.x, player.pos.y),
+      createVector(player.pos.x + player.w, player.pos.y),
+      createVector(player.pos.x, player.pos.y + player.h),
+      createVector(player.pos.x + player.w, player.pos.y + player.h)
+    ];
+    for (let c of corners) {
+      if (this.pointInTriangle(c, this.verts[0], this.verts[1], this.verts[2])) {
+        player.reset();
+        return;
+      }
+    }
+  }
+
+  // barybashing
+  pointInTriangle(p, a, b, c) {
+    let v0 = p5.Vector.sub(c, a);
+    let v1 = p5.Vector.sub(b, a);
+    let v2 = p5.Vector.sub(p, a);
+
+    let dot00 = v0.dot(v0);
+    let dot01 = v0.dot(v1);
+    let dot02 = v0.dot(v2);
+    let dot11 = v1.dot(v1);
+    let dot12 = v1.dot(v2);
+
+    let invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
+    let u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+    let v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+
+    return (u >= 0) && (v >= 0) && (u + v < 1);
   }
 }
